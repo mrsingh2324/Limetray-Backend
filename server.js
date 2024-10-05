449239bb07649750058ea5cc5f113117785de9a6
Satyam Singh <87685197+mrsingh2324@users.noreply.github.com>
2024-10-05 17:30:43 +0530

    updated cors

--- server.js
+++ server.js
@@ -2,14 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const allowedOrigins = [
-  'http://localhost:3000', 
-  'https://limetray-project-vqzy.vercel.app'
-];
-
-app.use(cors({
-  origin: allowedOrigins,
-}));
+app.use(cors());
 
 app.use(express.json());
 
